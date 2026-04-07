ca1b3abb463e5d2df781b0b6e5538c55e4b255d9
Ricardo Sanginés <rickrebel@gmail.com>
2026-04-07 11:26:11 -0600

    FalÂtaba cambiar el número de versión

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeeko_xlsx_export',
-    version='2.1.1',
+    version='2.2.0',
     description='Declarative Django model → Excel export framework',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
